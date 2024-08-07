fda618b5ce2437074aa0e6acb8fea50060217742
JinwonJ <93890608+JinwonJ@users.noreply.github.com>
2024-08-07 18:24:40 +0900

    jinwon introduction first git push

---
+++ src/Pages/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const Header = () => {
+    return (
+        <div id="Header">
+            ? Header는 뭘로 해야 좋을 소개페이지 일까
+            
+        </div>
+    )
+}
+
+export default Header
