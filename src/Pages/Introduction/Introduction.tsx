e01c0f27ed9f79e61d46292c7caac6a431292b43
JinwonJ <93890608+JinwonJ@users.noreply.github.com>
2024-08-21 15:47:42 +0900

    introduction page demo

--- src/Pages/Introduction/Introduction.tsx
+++ src/Pages/Introduction/Introduction.tsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Introduction.scss";
 
 const Introduction = () => {
+  useEffect(() => {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div id="Introduction">
       <div className="section section1">
         <div className="content">
-          <h2>자기소개</h2>
-          <ul>
-            <li>이러한 저러한 목표</li>
-            <li>이러한 이러한 작업을 좋아함</li>
-            <li>이러한 이러한것을 포용할줄 앎</li>
-            <li>적은 기간의 백엔드 경력이지만 관련 업무 이해도가 어느정도 있어 작업이 원활</li>
-          </ul>
+          <h2>About me</h2>
+          <li>풀스택을 꿈꾸는 개발자</li>
+          <li>내가 몸담는 회사의 색이 있는걸 좋아하는 개발자</li>
+          <li>개발 관련 요청을 잘받는 개발자</li>
+          <li>적지만 백앤드도 해본 개발자</li>
         </div>
       </div>
 
       <div className="section section2">
         <div className="content">
-          <h2>경력</h2>
+          <h2>Work Experience</h2>
           <ul>
             <li>백엔드(2020.12 ~ 2022.06) Python을 이용한 회원 관리 위주의 작업</li>
             <li>프론트엔드(2022.06 ~ 2024.07) React기반의 각종 페이지 제작</li>
@@ -28,10 +32,10 @@ const Introduction = () => {
 
       <div className="section section3">
         <div className="content">
-          <h2>작업물</h2>
+          <h2>Work Product</h2>
           <ul>
-            <li>이러한 이러한것을 포용할줄 앎</li>
-            <li>이러한 이러한것을 포용할줄 앎</li>
+            <li>서비스의 지도 기반 페이지 개발</li>
+            <li>각종 프로모션 페이지 제작</li>
           </ul>
         </div>
       </div>
