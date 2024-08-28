002f4329cc9e2de9a474633344ef10ae819d95fd
JinwonJ <93890608+JinwonJ@users.noreply.github.com>
2024-08-28 12:29:47 +0900

    mediaquery

--- src/MediaQuery/MideaQuery.tsx
+++ src/MediaQuery/MideaQuery.tsx
@@ -1,19 +1,44 @@
-import React from "react";
+// import React from "react";
+// import { useMediaQuery } from "react-responsive";
+
+// const Mobile: React.FC = ({ children }: any) => {
+//   const isMobile = useMediaQuery({
+//     query: "(max-width:767px)",
+//   });
+
+//   return <React.Fragment>{isMobile && children}</React.Fragment>;
+// };
+
+// const PC: React.FC = ({ children }: any) => {
+//   const isPc = useMediaQuery({
+//     query: "(min-width:768px) ",
+//   });
+//   return <React.Fragment>{isPc && children}</React.Fragment>;
+// };
+
+// export { Mobile, PC };
+
+import React, { ReactNode } from "react";
 import { useMediaQuery } from "react-responsive";
 
-const Mobile: React.FC = ({ children }: any) => {
+interface ResponsiveProps {
+  children: ReactNode;
+}
+
+const Mobile: React.FC<ResponsiveProps> = ({ children }) => {
   const isMobile = useMediaQuery({
     query: "(max-width:767px)",
   });
 
-  return <React.Fragment>{isMobile && children}</React.Fragment>;
+  return <>{isMobile && children}</>;
 };
 
-const PC: React.FC = ({ children }: any) => {
+const PC: React.FC<ResponsiveProps> = ({ children }) => {
   const isPc = useMediaQuery({
-    query: "(min-width:768px) ",
+    query: "(min-width:768px)",
   });
-  return <React.Fragment>{isPc && children}</React.Fragment>;
+
+  return <>{isPc && children}</>;
 };
 
 export { Mobile, PC };
